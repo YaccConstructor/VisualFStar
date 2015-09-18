1954d7a0ea11917f5ca5ac987ff2bfde40fc1065
rsdpisuy <rsdpisuy@gmail.com>
2015-09-18 15:30:57 +0300

    Basic version of language service.

---
+++ VisualFStar/FStarLanguageService.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Package;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.OLE.Interop;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+
+namespace VisualFStar
+{
+    [Microsoft.VisualStudio.Shell.ProvideService(typeof(FStarLanguageService))]
+    [Microsoft.VisualStudio.Shell.ProvideLanguageExtension(typeof(FStarLanguageService), ".fst")]
+    [Microsoft.VisualStudio.Shell.ProvideLanguageService(typeof(FStarLanguageService), "FStar Language", 0,
+        AutoOutlining = true,
+        EnableCommenting = true,
+        MatchBraces = true,
+        ShowMatchingBrace = true)]
+    [Guid("00000000-0000-0000-0000-000000000010")]
+    public class TestLanguagePackage : Package, IOleComponent
+    {
+        private uint m_componentID;
+
+
+        protected override void Initialize()
+        {
+            base.Initialize();  // required
+
+            // Proffer the service.
+            IServiceContainer serviceContainer = this as IServiceContainer;
+            FStarLanguageService langService = new FStarLanguageService();
+            langService.SetSite(this);
+            serviceContainer.AddService(typeof(FStarLanguageService),
+                                        langService,
+                                        true);
+
+            // Register a timer to call our language service during
+            // idle periods.
+            IOleComponentManager mgr = GetService(typeof(SOleComponentManager))
+                                       as IOleComponentManager;
+            if (m_componentID == 0 && mgr != null)
+            {
+                OLECRINFO[] crinfo = new OLECRINFO[1];
+                crinfo[0].cbSize = (uint)Marshal.SizeOf(typeof(OLECRINFO));
+                crinfo[0].grfcrf = (uint)_OLECRF.olecrfNeedIdleTime |
+                                              (uint)_OLECRF.olecrfNeedPeriodicIdleTime;
+                crinfo[0].grfcadvf = (uint)_OLECADVF.olecadvfModal |
+                                              (uint)_OLECADVF.olecadvfRedrawOff |
+                                              (uint)_OLECADVF.olecadvfWarningsOff;
+                crinfo[0].uIdleTimeInterval = 1000;
+                int hr = mgr.FRegisterComponent(this, crinfo, out m_componentID);
+            }
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (m_componentID != 0)
+            {
+                IOleComponentManager mgr = GetService(typeof(SOleComponentManager))
+                                           as IOleComponentManager;
+                if (mgr != null)
+                {
+                    int hr = mgr.FRevokeComponent(m_componentID);
+                }
+                m_componentID = 0;
+            }
+
+            base.Dispose(disposing);
+        }
+
+
+        #region IOleComponent Members
+
+        public int FDoIdle(uint grfidlef)
+        {
+            bool bPeriodic = (grfidlef & (uint)_OLEIDLEF.oleidlefPeriodic) != 0;
+            // Use typeof(TestLanguageService) because we need to
+            // reference the GUID for our language service.
+            LanguageService service = GetService(typeof(FStarLanguageService))
+                                      as LanguageService;
+            if (service != null)
+            {
+                service.OnIdle(bPeriodic);
+            }
+            return 0;
+        }
+
+        public int FContinueMessageLoop(uint uReason,
+                                        IntPtr pvLoopData,
+                                        MSG[] pMsgPeeked)
+        {
+            return 1;
+        }
+
+        public int FPreTranslateMessage(MSG[] pMsg)
+        {
+            return 0;
+        }
+
+        public int FQueryTerminate(int fPromptUser)
+        {
+            return 1;
+        }
+
+        public int FReserved1(uint dwReserved,
+                              uint message,
+                              IntPtr wParam,
+                              IntPtr lParam)
+        {
+            return 1;
+        }
+
+        public IntPtr HwndGetWindow(uint dwWhich, uint dwReserved)
+        {
+            return IntPtr.Zero;
+        }
+
+        public void OnActivationChange(IOleComponent pic,
+                                       int fSameComponent,
+                                       OLECRINFO[] pcrinfo,
+                                       int fHostIsActivating,
+                                       OLECHOSTINFO[] pchostinfo,
+                                       uint dwReserved)
+        {
+        }
+
+        public void OnAppActivate(int fActive, uint dwOtherThreadID)
+        {
+        }
+
+        public void OnEnterState(uint uStateID, int fEnter)
+        {
+        }
+
+        public void OnLoseActivation()
+        {
+        }
+
+        public void Terminate()
+        {
+        }
+
+        #endregion
+}
+
+    public class FStarLanguageService : LanguageService
+    {
+        private LanguagePreferences m_preferences;
+        private TestScanner m_scanner;
+
+        public override LanguagePreferences GetLanguagePreferences()
+        {
+            if (m_preferences == null)
+            {
+                m_preferences = new LanguagePreferences(this.Site,
+                                                        typeof(FStarLanguageService).GUID,
+                                                        this.Name);
+                m_preferences.Init();
+            }
+            return m_preferences;
+        }
+        
+        public override IScanner GetScanner(IVsTextLines buffer)
+        {
+            if (m_scanner == null)
+            {
+                m_scanner = new TestScanner(buffer);
+            }
+            return m_scanner;
+        }
+
+        public override AuthoringScope ParseSource(ParseRequest req)
+        {
+            return new TestAuthoringScope();
+        }
+
+        public override string Name
+        {
+            get { return "FStar"; }
+        }
+
+        public override string GetFormatFilterList()
+        {
+            return "*.fst";
+        }
+    }
+}
+
+internal class TestScanner : IScanner
+{
+    private IVsTextBuffer m_buffer;
+    string m_source;
+
+    public TestScanner(IVsTextBuffer buffer)
+    {
+        m_buffer = buffer;
+    }
+
+    bool IScanner.ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
+    {
+        tokenInfo.Type = TokenType.Unknown;
+        tokenInfo.Color = TokenColor.Text;
+        return true;
+    }
+
+    void IScanner.SetSource(string source, int offset)
+    {
+        m_source = source.Substring(offset);
+    }
+}
+
+internal class TestAuthoringScope : AuthoringScope
+{
+    public override string GetDataTipText(int line, int col, out TextSpan span)
+    {
+        span = new TextSpan();
+        return null;
+    }
+
+    public override Declarations GetDeclarations(IVsTextView view,
+                                                 int line,
+                                                 int col,
+                                                 TokenInfo info,
+                                                 ParseReason reason)
+    {
+        return null;
+    }
+
+    public override string Goto(VSConstants.VSStd97CmdID cmd, IVsTextView textView, int line, int col, out TextSpan span)
+    {
+        span = new TextSpan();
+        return null;
+    }
+
+    public override Methods GetMethods(int line, int col, string name)
+    {
+        return null;
+    }
+}
