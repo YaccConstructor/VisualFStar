1d541303fb58deb4018347a3f1b9be4b9c58a5cd
rsdpisuy <rsdpisuy@gmail.com>
2015-09-18 15:35:18 +0300

    Move Package Initialize to right place.

--- VisualFStar/FStarVSPackage.cs
+++ VisualFStar/FStarVSPackage.cs
@@ -15,6 +15,7 @@ using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.Win32;
+using Microsoft.VisualStudio.Package;
 
 namespace VisualFStar
 {
@@ -35,11 +36,18 @@ namespace VisualFStar
     /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
     /// </para>
     /// </remarks>
+    [Microsoft.VisualStudio.Shell.ProvideService(typeof(FStarLanguageService))]
+    [Microsoft.VisualStudio.Shell.ProvideLanguageExtension(typeof(FStarLanguageService), ".fst")]
+    [Microsoft.VisualStudio.Shell.ProvideLanguageService(typeof(FStarLanguageService), "FStar Language", 0,
+        AutoOutlining = true,
+        EnableCommenting = true,
+        MatchBraces = true,
+        ShowMatchingBrace = true)]
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(FStarVSPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    public sealed class FStarVSPackage : Package
+    public sealed class FStarVSPackage : Package, IOleComponent
     {
         /// <summary>
         /// FStarVSPackage GUID string.
@@ -63,11 +71,129 @@ namespace VisualFStar
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
         /// </summary>
+        private uint m_componentID;
+
+
         protected override void Initialize()
         {
-            base.Initialize();
+            base.Initialize();  // required
+
+            // Proffer the service.
+            IServiceContainer serviceContainer = this as IServiceContainer;
+            FStarLanguageService langService = new FStarLanguageService();
+            langService.SetSite(this);
+            serviceContainer.AddService(typeof(FStarLanguageService),
+                                        langService,
+                                        true);
+
+            // Register a timer to call our language service during
+            // idle periods.
+            IOleComponentManager mgr = GetService(typeof(SOleComponentManager))
+                                       as IOleComponentManager;
+            if (m_componentID == 0 && mgr != null)
+            {
+                OLECRINFO[] crinfo = new OLECRINFO[1];
+                crinfo[0].cbSize = (uint)Marshal.SizeOf(typeof(OLECRINFO));
+                crinfo[0].grfcrf = (uint)_OLECRF.olecrfNeedIdleTime |
+                                              (uint)_OLECRF.olecrfNeedPeriodicIdleTime;
+                crinfo[0].grfcadvf = (uint)_OLECADVF.olecadvfModal |
+                                              (uint)_OLECADVF.olecadvfRedrawOff |
+                                              (uint)_OLECADVF.olecadvfWarningsOff;
+                crinfo[0].uIdleTimeInterval = 1000;
+                int hr = mgr.FRegisterComponent(this, crinfo, out m_componentID);
+            }
         }
 
+
+        protected override void Dispose(bool disposing)
+        {
+            if (m_componentID != 0)
+            {
+                IOleComponentManager mgr = GetService(typeof(SOleComponentManager))
+                                           as IOleComponentManager;
+                if (mgr != null)
+                {
+                    int hr = mgr.FRevokeComponent(m_componentID);
+                }
+                m_componentID = 0;
+            }
+
+            base.Dispose(disposing);
+        }
+
+
+        #region IOleComponent Members
+
+        public int FDoIdle(uint grfidlef)
+        {
+            bool bPeriodic = (grfidlef & (uint)_OLEIDLEF.oleidlefPeriodic) != 0;
+            // Use typeof(TestLanguageService) because we need to
+            // reference the GUID for our language service.
+            LanguageService service = GetService(typeof(FStarLanguageService))
+                                      as LanguageService;
+            if (service != null)
+            {
+                service.OnIdle(bPeriodic);
+            }
+            return 0;
+        }
+
+        public int FContinueMessageLoop(uint uReason,
+                                        IntPtr pvLoopData,
+                                        MSG[] pMsgPeeked)
+        {
+            return 1;
+        }
+
+        public int FPreTranslateMessage(MSG[] pMsg)
+        {
+            return 0;
+        }
+
+        public int FQueryTerminate(int fPromptUser)
+        {
+            return 1;
+        }
+
+        public int FReserved1(uint dwReserved,
+                              uint message,
+                              IntPtr wParam,
+                              IntPtr lParam)
+        {
+            return 1;
+        }
+
+        public IntPtr HwndGetWindow(uint dwWhich, uint dwReserved)
+        {
+            return IntPtr.Zero;
+        }
+
+        public void OnActivationChange(IOleComponent pic,
+                                       int fSameComponent,
+                                       OLECRINFO[] pcrinfo,
+                                       int fHostIsActivating,
+                                       OLECHOSTINFO[] pchostinfo,
+                                       uint dwReserved)
+        {
+        }
+
+        public void OnAppActivate(int fActive, uint dwOtherThreadID)
+        {
+        }
+
+        public void OnEnterState(uint uStateID, int fEnter)
+        {
+        }
+
+        public void OnLoseActivation()
+        {
+        }
+
+        public void Terminate()
+        {
+        }
+
+        #endregion
         #endregion
     }
 }
