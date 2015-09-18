1b047ccdac008f6e9917c72c5f95fef9c53f119c
rsdpisuy <rsdpisuy@gmail.com>
2015-09-18 12:51:37 +0300

    Basic solution initialization.

---
+++ src/VisualFStar/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("VisualFStar")>]
+[<assembly: AssemblyProductAttribute("VisualFStar")>]
+[<assembly: AssemblyDescriptionAttribute("Support for F* in Visual Studio IDE")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
