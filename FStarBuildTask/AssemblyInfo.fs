37457aa2559c9e7d9969018eb0a41aa0aa147f50
ragozina.anastasiya <ragozina.anastasiya@gmail.com>
2015-10-01 16:08:58 +0300

    Add new project

---
+++ FStarBuildTask/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace FStarBuildTask.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FStarBuildTask")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FStarBuildTask")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("f810a9d0-cd24-4824-8e21-3e0633427432")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ FStarBuildTask/FStarBuildTask.fs
@@ -0,0 +1,10 @@
+﻿module FStarBuildTask
+open FStar
+open FStar.Absyn
+open FStar.Absyn.Syntax
+open FStar.Util
+open FStar.Getopt
+open FStar.Tc.Util
+open FStar.Tc.Env
+
+let x = 1
