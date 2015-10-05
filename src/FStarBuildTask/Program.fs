5abb65dc4c48608635134264dde0843590b69386
ragozina.anastasiya <ragozina.anastasiya@gmail.com>
2015-10-05 12:25:38 +0300

    Add z3, move project

---
+++ src/FStarBuildTask/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help
+module FStarBuildProject
+open FStar.FStar
+[<EntryPoint>]
+let main str = 
+    FStar.FStar.goInternal "s"
+    0
+
+
+    
