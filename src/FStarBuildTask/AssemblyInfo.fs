0f15c0cd07df59e02accb62defb1877fdb3a1180
rsdpisuy <rsdpisuy@gmail.com>
2015-10-23 11:18:32 +0300

    Attempt to fix project items.

--- src/FStarBuildTask/AssemblyInfo.fs
+++ src/FStarBuildTask/AssemblyInfo.fs
@@ -1,41 +1,12 @@
-﻿namespace FStarBuildTask.AssemblyInfo
-
+﻿namespace System
 open System.Reflection
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[<assembly: AssemblyTitle("FStarBuildTask")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("FStarBuildTask")>]
-[<assembly: AssemblyCopyright("Copyright ©  2015")>]
-[<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyTitleAttribute("FStarBuildTask")>]
+[<assembly: AssemblyProductAttribute("VisualFStar")>]
+[<assembly: AssemblyDescriptionAttribute("Support for F* in Visual Studio IDE")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("f810a9d0-cd24-4824-8e21-3e0633427432")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
-
-do
-    ()
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
